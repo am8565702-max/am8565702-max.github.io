5aa528d08a4d9b9ff44a0f7a6532466c710fd04f
am8565702-max <am8565702@gmail.com>
2026-08-01 21:27:47 +0400

    Refresh scheduling and admin cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "olive-branch-menu-v32";
+var CACHE_NAME = "olive-branch-menu-v33";
 var STATIC_FILES = [
   "./",
   "./index.html",
@@ -7,8 +7,8 @@ var STATIC_FILES = [
   "./icon-512.png",
   "./apple-touch-icon.png",
   "./order-receipt.js?v=7-performance-fix",
-  "./menu-enhancements.css?v=3-status-sync",
-  "./menu-enhancements.js?v=5-tracking-source"
+  "./menu-enhancements.css?v=6-admin-fields",
+  "./menu-enhancements.js?v=6-admin-fields"
 ];
 
 self.addEventListener("install", function (event) {
